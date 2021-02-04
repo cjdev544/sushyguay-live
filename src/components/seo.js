5ac8c2c058759d289e06683b174be2ff290eb2fa
cjdev544 <60110377+cjdev544@users.noreply.github.com>
2021-02-04 18:49:49 -0400

    Update seo.js
    cambio de title en SEO

--- src/components/seo.js
+++ src/components/seo.js
@@ -33,7 +33,7 @@ function SEO({ description, lang, meta, title }) {
         lang,
       }}
       title={title}
-      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+      titleTemplate={defaultTitle ? `%s | ${title}` : null}
       meta={[
         {
           name: `description`,
